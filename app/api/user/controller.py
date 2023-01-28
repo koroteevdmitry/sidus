539e53ec96538372c7475e2a2748140f8336fe1e
koroteev-dmitry <desing94root@gmail.com>
2023-01-28 15:33:15 +0300

    Cache result get user method

--- app/api/user/controller.py
+++ app/api/user/controller.py
@@ -2,12 +2,15 @@ from http import HTTPStatus
 from typing import Optional
 from uuid import UUID
 
+from dependency_injector.wiring import inject, Provide
 from fastapi import APIRouter, Depends, HTTPException, Form
 from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
 from api.user.schemas import UserCreateSchema, UserSchema, UserUpdateSchema
 from api.user.services import UserService
+from core.di.container import Container
+from core.redis import AsyncRedis
 from core.utils import verify_password
 from db.models.user import User
 
@@ -29,14 +32,23 @@ class UpdateUserRequestPayload(BaseModel):
     last_name: Optional[str]
 
 
-@router.get("/uid/", response_model=UserSchema)
-async def get_user(uid: UUID, service: UserService = Depends()) -> User:
+@router.get('/uid/', response_model=UserSchema)
+@inject
+async def get_user(
+    uid: UUID,
+    redis: AsyncRedis = Depends(Provide[Container.redis_service]),
+    service: UserService = Depends(),
+) -> User:
     if not (user := await service.get_user_by_id(uid)):
         raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail='No such user found.')
-    return user
+    if not (value := await redis.get(str(uid), dict_value=True)):
+        await redis.set(str(uid), user.dict())
+        return user
+    else:
+        return User.parse_obj(value)
 
 
-@router.post("/", response_model=UserSchema)
+@router.post('/', response_model=UserSchema)
 async def create_user(payload: CreateUserRequestPayload, service: UserService = Depends()) -> User:
     if await service.get_user_by_email(payload.email):
         raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail='User with this email already exists.')
@@ -63,14 +75,16 @@ async def _authentication(form_data: OAuth2PasswordRequestForm = Depends(), serv
     return user
 
 
-@router.put("/uid/", response_model=UserSchema)
+@router.put('/uid/', response_model=UserSchema)
+@inject
 async def update_user(
     uid: UUID,
     email: Optional[str] = Form(None),
     fist_name: Optional[str] = Form(None),
     last_name: Optional[str] = Form(None),
     service: UserService = Depends(),
-    auth_user: User = Depends(_authentication)
+    auth_user: User = Depends(_authentication),
+    redis: AsyncRedis = Depends(Provide[Container.redis_service]),
 ) -> User:
     payload = UpdateUserRequestPayload(
         email=email,
@@ -82,4 +96,5 @@ async def update_user(
     if await service.get_user_by_email(payload.email) and auth_user.email != payload.email:
         raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY, detail='User with this email already exists.')
 
+    await redis.delete(str(uid))
     return await service.update_and_return_user(uid, UserUpdateSchema.create(**payload.dict(exclude_none=True)))
